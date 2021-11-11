4edfea0b786d1b5791e00f1f26c5d2a85aee7f5e
FLAFAV <farrel.ballard@ti.ukdw.ac.id>
2021-11-11 19:44:38 +0700

    Menambahkan jawaban 2

---
+++ Desktop/UG9_A_71210679/2_A_71210679.py
@@ -0,0 +1,6 @@
+#Buatlah program sederhana yang bisa menghitung perpangkatan 5. 
+#Diberikan input agar user bisa menginput angka untuk dipangkatkan.
+
+x = int(input("Masukkan angka: "))
+y = x**5
+print(x, "pangkat 5 adalah", y)
