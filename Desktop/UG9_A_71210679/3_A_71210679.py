87527a2816d59dbbce2f11adc055b73d8300c41c
FLAFAV <farrel.ballard@ti.ukdw.ac.id>
2021-11-11 19:45:59 +0700

    Menambahkan jawaban 3

---
+++ Desktop/UG9_A_71210679/3_A_71210679.py
@@ -0,0 +1,10 @@
+#Doni sedang berbelanja dengan membawa catatan digital yang dia buat sendiri dan 
+#catatan itu sudah diisi oleh ibunya. Namun di program Doni tidak bisa menambahkan catatan lagi.
+#Buatlah fitur untuk menambah catatan di program Doni.
+
+a = input("Masukkan daftar belanja 1: ")
+b = input("Masukkan daftar belanja 2: ")
+c = input("Tambahkan data ke daftar belanja 1: ")
+d = input("Tambahkan data ke daftar belanja 2: ")
+print("daftar belanja 1 adalah", a,",", c)
+print("daftar belanja 2 adalah", b,",", d)
