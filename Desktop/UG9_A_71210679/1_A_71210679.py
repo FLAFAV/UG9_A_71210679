f2d45a3dbb55380efeb520b8f41886abe13329ff
FLAFAV <farrel.ballard@ti.ukdw.ac.id>
2021-11-11 19:42:29 +0700

    Menambahkan jawaban 1

---
+++ Desktop/UG9_A_71210679/1_A_71210679.py
@@ -0,0 +1,8 @@
+#Doni ingin membuat lintasan lari berbentuk keliling lingkaran. 
+#Dia ingin tahu keliling lingkaran yang akan dibuat tetapi Doni tidak bisa menghitung. 
+#Bantulah Doni membuat program sederhana untuk menghitung keliling lingkaran!
+
+diameter = int(input("Masukkan diameter:"))
+pi = 3.14
+rumus = diameter*pi
+print("Keliling lingkaran adalah: ",round(rumus,2))
